577a7b64419989b8e40db746fce77dbefaecc869
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-08-16 11:34:03 -0300

    Correção nas estatísticas que apenas somavam

--- js/main.js
+++ js/main.js
@@ -2,42 +2,42 @@ const controle = document.querySelectorAll("[data-controle]");
 const estatisticas = document.querySelectorAll("[data-estatistica]");
 const pecas = {
   "bracos": {
-      "forca": 29,
-      "poder": 35,
-      "energia": -21,
-      "velocidade": -5
+    "forca": 29,
+    "poder": 35,
+    "energia": -21,
+    "velocidade": -5
   },
 
   "blindagem": {
-      "forca": 41,
-      "poder": 20,
-      "energia": 0,
-      "velocidade": -20
+    "forca": 41,
+    "poder": 20,
+    "energia": 0,
+    "velocidade": -20
   },
-  "nucleos":{
-      "forca": 0,
-      "poder": 7,
-      "energia": 48,
-      "velocidade": -4
+  "nucleos": {
+    "forca": 0,
+    "poder": 7,
+    "energia": 48,
+    "velocidade": -4
   },
-  "pernas":{
-      "forca": 27,
-      "poder": 21,
-      "energia": -32,
-      "velocidade": 43
+  "pernas": {
+    "forca": 27,
+    "poder": 21,
+    "energia": -32,
+    "velocidade": 43
   },
-  "foguetes":{
-      "forca": 0,
-      "poder": 28,
-      "energia": 0,
-      "velocidade": -2
+  "foguetes": {
+    "forca": 0,
+    "poder": 28,
+    "energia": 0,
+    "velocidade": -2
   }
 };
 
-controle.forEach( (elemento) => {
+controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
     manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
-    atualizaEstatisticas(evento.target.dataset.peca);
+    atualizaEstatisticas(evento.target.dataset.peca, evento.target.dataset.controle);
   })
 });
 
@@ -51,8 +51,14 @@ function manipulaDados(operacao, controle) {
   }
 };
 
-function atualizaEstatisticas(peca) {
-  estatisticas.forEach( (elemento) => {
-    elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
-  });
+function atualizaEstatisticas(peca, operacao) {
+  if (operacao === "+") {
+    estatisticas.forEach((elemento) => {
+      elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+    })
+  } else {
+    estatisticas.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+      })
+    }
 };
